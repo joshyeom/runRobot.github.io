d401fe520785389dbd15b81f15549c20293f4c01
joshyeom <jhyeom95@daum.net>
2023-05-10 12:18:04 +0900

    test for Safari

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const dataUrl = "./data.json";
 const currentUrl = window.location.href;
-const regex = /index[<>]?d+/;
+const regex = /index(\d+)\.html/;
 const currentPageMatch = currentUrl.match(regex);
-const currentPage = currentPageMatch
-  ? parseInt(currentPageMatch[0].slice(5))
-  : 1;
+let currentPage = currentPageMatch ? parseInt(currentPageMatch[0].slice(5)) : 1;
 
 function displayContent(data, container, propertyKeys, index) {
   if (index >= propertyKeys.length) {
@@ -76,14 +74,34 @@ xhr.send();
 const beforeButton = document.querySelector(".left");
 const nextButton = document.querySelector(".right");
 
-beforeButton.addEventListener("click", handlePage);
-nextButton.addEventListener("click", handlePage);
+if (currentPage === 1) {
+  beforeButton.style.display = "none";
+} else if (currentPage === 3) {
+  nextButton.style.display = "none";
+}
 
-function handlePage(event) {
-  const currentDirection = event.target.classList.contains("left") ? -1 : 1;
-  const newPage = currentPage + currentDirection;
+beforeButton.addEventListener("click", function () {
+  handlePage(-1);
+});
+nextButton.addEventListener("click", function () {
+  handlePage(1);
+});
+
+function handlePage(direction) {
+  const newPage = currentPage + direction;
   if (newPage < 1 || newPage > 3) {
     return;
   }
-  window.location.href = `./index${newPage}.html`;
+
+  // 현재 페이지의 URL에서 숫자 부분을 추출
+  const regex = /index([0-9]+)\.html/;
+  const match = window.location.href.match(regex);
+  const currentPageNumber = match ? parseInt(match[1]) : 1;
+
+  // 다음 페이지의 URL 생성
+  const nextPageNumber = currentPageNumber + direction;
+  const nextPageUrl = `./index${nextPageNumber}.html`;
+
+  window.location.href = nextPageUrl;
+  currentPage = newPage; // currentPage 업데이트
 }
